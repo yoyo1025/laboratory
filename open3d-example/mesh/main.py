9755e4b0646e5ba0e65c290478b6ef5c7823609c
yoyo1025 <hsjejxys@gmail.com>
2025-06-16 17:59:19 +0900

    コメント追加

--- open3d-example/mesh/main.py
+++ open3d-example/mesh/main.py
@@ -20,11 +20,13 @@ th = np.quantile(densities, 0.025)   # 下位 2.5% をカット
 mesh.remove_vertices_by_mask(densities < th)
 
 mesh = (
-    mesh.remove_duplicated_vertices()
-        .remove_degenerate_triangles()
-        .remove_non_manifold_edges()
-        .remove_unreferenced_vertices()
+    mesh
+    .remove_duplicated_vertices()         # 【重複頂点の除去】完全に同じ座標を持つ頂点を1つにまとめる → meshの軽量化と後続処理の安定化
+    .remove_degenerate_triangles()        # 【縮退三角形の除去】頂点が重複していて面積がゼロに近い三角形（線または点状）を削除
+    .remove_non_manifold_edges()         # 【非多様体エッジ処理】3面以上にまたがるエッジを検出し、最も面積の小さい三角形から順に削除してエッジを通常の形状に整える :contentReference[oaicite:1]{index=1}
+    .remove_unreferenced_vertices()      # 【未参照頂点の除去】三角形に使われなくなった頂点を削除して、無駄な頂点を整理 :contentReference[oaicite:2]{index=2}
 )
+
 
 # --- 必要なら軽量化 --------------------------
 mesh = mesh.simplify_vertex_clustering(voxel_size=0.0025)
