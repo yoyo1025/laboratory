e5cb70a44082856673eae4c0417162d2097c80d2
yoyo1025 <hsjejxys@gmail.com>
2025-07-10 13:57:37 +0900

    fix: Geohashの桁数を定数化

--- poc1/app/main.py
+++ poc1/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, Form
 import pygeohash
 
+# Geohashの桁数
+GEOHASH_LEVEL = 8
+
 app = FastAPI()
 
 
@@ -10,6 +13,6 @@ def health():
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...), lat: str = Form(), lon: str = Form()):
-    geohash = pygeohash.encode(latitude=float(lat), longitude=float(lon), precision=8)
+    geohash = pygeohash.encode(latitude=float(lat), longitude=float(lon), precision=GEOHASH_LEVEL)
     contents = await file.read()
     return {"filename": file.filename, "lat": lat, "lon": lon, "geohash": geohash, "contents": contents}
