43bad6eb57d1db0d795d65e9be212710a79f0cfc
yoyo1025 <hsjejxys@gmail.com>
2025-07-10 14:16:20 +0900

    add: レスポンスモデル定義

--- poc1/app/main.py
+++ poc1/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import pygeohash
 
 # Geohashの桁数
@@ -6,13 +8,26 @@ GEOHASH_LEVEL = 8
 
 app = FastAPI()
 
+class UploadResponse(BaseModel):
+    filename: str
+    lat: str
+    lon: str
+    geohash: str
+    contents: str
+
 
 @app.get("/health")
 def health():
     return {"Hello": "World"}
 
-@app.post("/upload")
+@app.post("/upload", response_model=UploadResponse)
 async def upload(file: UploadFile = File(...), lat: str = Form(), lon: str = Form()):
     geohash = pygeohash.encode(latitude=float(lat), longitude=float(lon), precision=GEOHASH_LEVEL)
     contents = await file.read()
-    return {"filename": file.filename, "lat": lat, "lon": lon, "geohash": geohash, "contents": contents}
+    return {
+        "filename": file.filename,
+        "lat": lat,
+        "lon": lon,
+        "geohash": geohash,
+        "contents": str(contents),
+    }
