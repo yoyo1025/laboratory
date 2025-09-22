d10fab2fb6a17d7aee6889c82a68ed35026fceef
yoyo1025 <hsjejxys@gmail.com>
2025-09-22 14:36:58 +0900

    feat: tmpフォルダから{geohash}フォルダへ点群データを移動

--- poc1/cloud/app/main.py
+++ poc1/cloud/app/main.py
@@ -3,9 +3,9 @@ import os
 from urllib.parse import unquote
 import logging
 from minio import Minio
-from usecase.aligmnent_usecase import AligmentUsecase  
 import tempfile
 import open3d as o3d
+from usecase.point_cloud_usecase import PointCloudUsecase
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -15,7 +15,7 @@ MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "cloud-minio:9100")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minio_root")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minio_password")
 MINIO_SECURE = os.getenv("MINIO_SECURE", "false").lower() == "true"
-    
+     
 mc = Minio(MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, secure=MINIO_SECURE)
 
 def handle_record_sync(rec, mc: Minio):
@@ -23,10 +23,13 @@ def handle_record_sync(rec, mc: Minio):
     bucket = s3.get("bucket", {}).get("name")
     key = s3.get("object", {}).get("key")
     event = rec.get("eventName", "")
-
-    print("INFO: bucket:", bucket)
-    print("INFO: object key:", key)
-    print("INFO: event:", event)
+    
+    # 文字化け対策
+    if not bucket or not key:
+        return
+    key = unquote(key)
+    
+    PointCloudUsecase(mc, s3).save(key)
 
 @app.post("/minio/webhook")
 async def PCLocalAlignmentHandler(request: Request, background: BackgroundTasks):
