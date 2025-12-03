5ea1a76b387166efecb450212fa5f59cb106ae74
yoyo1025 <hsjejxys@gmail.com>
2025-12-03 15:05:11 +0900

    20rpsの結果保存

--- poc1/k6/combined.js
+++ poc1/k6/combined.js
@@ -33,7 +33,7 @@ export const options = {
   scenarios: {
     pointcloud_upload: {
       executor: 'constant-arrival-rate',
-      rate: 10,
+      rate: 20,
       timeUnit: '1s',
       duration: '3m',
       preAllocatedVUs: 50,
