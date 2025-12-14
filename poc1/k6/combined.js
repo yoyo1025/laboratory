b7eb59ea2b243351ddc8d3e6175ace1d14c12c85
yoyo1025 <hsjejxys@gmail.com>
2025-12-14 23:36:56 +0900

    提案手法 30rps結果

--- poc1/k6/combined.js
+++ poc1/k6/combined.js
@@ -34,7 +34,7 @@ export const options = {
     pointcloud_upload: {
       executor: 'constant-arrival-rate',
       rate: 1,
-      timeUnit: '0.05s',
+      timeUnit: '0.033333s',
       duration: '3m',
       preAllocatedVUs: 50,
       maxVUs: 2000,
