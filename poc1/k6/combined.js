eeaf5fbc6118faf2f330395ae2c7b2eea022182a
yoyo1025 <hsjejxys@gmail.com>
2026-01-06 16:25:11 +0900

    従来手法 構築再試験 150rps

--- poc1/k6/combined.js
+++ poc1/k6/combined.js
@@ -34,7 +34,7 @@ export const options = {
     pointcloud_upload: {
       executor: 'constant-arrival-rate',
       rate: 1,
-      timeUnit: '0.00769230s',
+      timeUnit: '0.00666666s',
       duration: '1m',
       preAllocatedVUs: 50,
       maxVUs: 2000,
