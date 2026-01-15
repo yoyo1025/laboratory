fbc4693b8dcd3d9b7447ca9d40fb483766858889
yoyo1025 <hsjejxys@gmail.com>
2026-01-16 00:45:08 +0900

    エッジクラウド型 エッジヒット率70% 70RPS 結果

--- poc1/k6/download.js
+++ poc1/k6/download.js
@@ -6,7 +6,7 @@ export const options = {
     pointcloud_fetch: {
       executor: 'constant-arrival-rate',
       rate: 1,
-      timeUnit: '0.02s',
+      timeUnit: '0.014285s',
       duration: '1m',
       preAllocatedVUs: 50,
       maxVUs: 2000,
